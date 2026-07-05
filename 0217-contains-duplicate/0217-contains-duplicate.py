41d02b9515957f64bf65159a3a210efdaabb6dce
ashish bisariya <ashish.240102286@iiitbh.ac.in>
2026-07-05 21:27:07 +0530

    Time: 13 ms (83.27%), Space: 25.8 MB (57.98%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,10 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        my_set = set()
-        for i in nums:
-            if i in my_set:
-                return True
-            else:
-                my_set.add(i)
-        return False
+        my_set = set(nums)
+        if len(nums) != len(my_set):
+            return True
+        else:
+            return False
