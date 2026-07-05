b5ede7f0640264881b110d578e76c1ed13e320b6
ashish bisariya <ashish.240102286@iiitbh.ac.in>
2026-07-05 21:25:48 +0530

    Time: 29 ms (21.67%), Space: 25.8 MB (57.98%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        my_set = set()
+        for i in nums:
+            if i in my_set:
+                return True
+            else:
+                my_set.add(i)
+        return False
