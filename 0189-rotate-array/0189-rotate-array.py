2ba17db2c57a71b2015628777dfaeb95f8c68440
ashish bisariya <ashish.240102286@iiitbh.ac.in>
2026-07-08 11:53:58 +0530

    Time: 0 ms (100%), Space: 23.4 MB (25.48%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,4 +1,10 @@
 class Solution(object):
     def rotate(self, nums, k):
-        k=k%len(nums)
-        nums[:]=nums[-k:]+nums[:-k]
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k%n
+        nums[:] = nums[-k:] + nums[:-k]
