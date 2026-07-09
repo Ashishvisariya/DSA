9073cb90169ff0021b174f8c9de5b1d4e57a9759
ashish bisariya <ashish.240102286@iiitbh.ac.in>
2026-07-09 23:03:12 +0530

    Time: 0 ms (100%), Space: 12.3 MB (50.31%) - LeetHub

--- 3959-check-good-integer/3959-check-good-integer.py
+++ 3959-check-good-integer/3959-check-good-integer.py
@@ -4,11 +4,18 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        num = str(n)
-        digit = 0
-        total = 0
-        for i in num:
-            total += int(i)**2
-            digit += int(i)
-        
-        return total - digit >= 50
+        li=[]
+        rem=0
+        while(n!=0):
+            rem=n%10
+            li.append(rem)
+            n=n/10
+        DigitSum=0
+        SSum=0
+        for i in li:
+            DigitSum+=i
+            SSum=SSum+(i**2)
+        if (SSum-DigitSum)>=50:
+            return True
+        else:
+            return False
