c7c0014cee86f91edf9edaaa7828b53ebfc7b825
ashish bisariya <ashish.240102286@iiitbh.ac.in>
2026-07-10 22:11:48 +0530

    Time: 0 ms (100%), Space: 12.3 MB (53.99%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,4 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        one = 0
+        zero = 0
+        two = 0
+        for i in nums:
+            if i == 0:
+                zero +=1
+            elif i == 1:
+                one +=1
+            else:
+                two +=1
+        for i in range(len(nums)):
+            if zero > 0:
+                nums[i] = 0
+                zero-=1
+                continue
+            elif one > 0:
+                nums[i] = 1
+                one-=1
+                continue
+            else:
+                nums[i] = 2
+
+
+     
+            
