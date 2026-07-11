619fc06c61ef182787201dac974bafeb37aa45ee
ashish bisariya <ashish.240102286@iiitbh.ac.in>
2026-07-11 23:50:20 +0530

    Time: 4 ms (79.79%), Space: 13.6 MB (73.1%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,8 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        my_list = {}
-        for i in nums:
-            my_list[i] = my_list.get(i,0)+1
-
-        return max(my_list ,key=my_list.get)
+        n = len(nums)
+        nums.sort()
+        n1 = n//2
+        return nums[n1]
