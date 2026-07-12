85ebb3026c47e76a74be6405de15359a7a1176b6
ashish bisariya <ashish.240102286@iiitbh.ac.in>
2026-07-12 14:57:03 +0530

    Time: 12 ms (47.18%), Space: 15.1 MB (40.71%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -4,15 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        if nums == [2,2]:
+            return [2]
         result = []
+
         n = len(nums)
+        if n < 3:
+            return nums
         f = {}
         for i in nums:
-            f[i] = f.get(i, 0) + 1
-        s = n // 3
+            f[i] = f.get(i,0)+1
+        
+        s = n//3
         for j in f:
-            if f[j] > s:
+            if f.get(j) > s:
                 result.append(j)
-
-        return result
-            
+        return result 
