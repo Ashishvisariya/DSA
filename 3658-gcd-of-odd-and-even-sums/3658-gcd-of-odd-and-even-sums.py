9b9eb50c1d3fe27ce9beb885664af4e3d7722c79
ashish bisariya <ashish.240102286@iiitbh.ac.in>
2026-07-16 00:31:33 +0530

    Time: 0 ms (100%), Space: 12.3 MB (61.75%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -4,24 +4,5 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        def gcd(a, b):
-            while b:
-                a, b = b, a % b
-            return a
-        e_sum = 0
-        o_sum = 0
-        e_count = 0
-        o_count = 0
-        i = 1
-        while o_count < n and e_count < n:
-            if i % 2 != 0:
-                o_sum += i
-                o_count+=1
-            else:
-                e_sum += i
-                e_count+=1
-            i+=1
-                
-        return gcd(e_sum,o_sum)
-
+        return n
         
