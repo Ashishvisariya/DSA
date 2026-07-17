ff74fbac207ae2553f79af3c0cd24d472f108d19
ashish bisariya <ashish.240102286@iiitbh.ac.in>
2026-07-17 17:07:17 +0530

    Time: 3 ms (54.27%), Space: 12.3 MB (91.46%) - LeetHub

--- 3866-first-unique-even-element/3866-first-unique-even-element.py
+++ 3866-first-unique-even-element/3866-first-unique-even-element.py
@@ -4,9 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        for i in nums:
-            if nums.count(i) == 1 and i % 2 == 0:
-                return i
+        n = len(nums)
+        m = [0] * 101
+
+        for num in nums:
+            m[num] += 1
         
+        for i in range(n):
+            if (nums[i] & 1) == 0 and m[nums[i]] == 1:
+                return nums[i]
+            
         return -1
-        
