953a04653106cafaeb5241c041685690714bf2fc
ashish bisariya <ashish.240102286@iiitbh.ac.in>
2026-07-26 10:43:41 +0530

    Time: 28 ms (67.61%), Space: 13.2 MB (31.03%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
@@ -5,8 +5,13 @@ class Solution(object):
         :rtype: int
         """
         nums.sort()
-        
-        return max(nums[0]*nums[1]*nums[-1],nums[-1]*nums[-2]*nums[-3])
+
+        n = len(nums)
+
+        return max(
+            nums[n-1] * nums[n-2] * nums[n-3],
+            nums[0] * nums[1] * nums[n-1]
+        )
 
             
         
